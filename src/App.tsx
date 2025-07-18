0f133f803ce771f2fc005ea03665ad0c66e2552a
KPPOKE <kptple@gmail.com>
2025-07-18 20:11:32 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { AuthProvider } from './context/AuthContext';
 import { useAuth } from './context/useAuth';
 import { LoyaltyProvider } from './context/LoyaltyContext';
